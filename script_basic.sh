0827cd93f561bc5bb3c317795401ed26fed71a04
Glauber GF <glauber_gf@hotmail.com>
2021-04-28 23:45:25 -0300

    Adicionado script_basic.sh ao projeto

---
+++ script_basic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#--- Mudar a senha do usuário Vagrant.
+#echo 'vagrant:zaq1' | sudo chpasswd
+
+# --- Atualizar e instalar pacotes no sistema.
+apt update
+apt install -y tree neofetch python3-pymysql python-apt
